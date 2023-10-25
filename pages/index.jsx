8d5a4a9762fa323f23178b5d113bbfda09386643
Joshua Waiswa <jwaiswa7@gmail.com>
2023-10-25 10:53:01 +0300

    Update the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,7 +21,7 @@ const Index2 = () => {
               <div
                 className="image-layer bg_cover"
                 style={{
-                  backgroundImage: "url(assets/images/hero/hero-two_img-1.jpg)",
+                  backgroundImage: "url(assets/images/hero/entrance.jpg)",
                 }}
               />
               <div className="container-fluid">
@@ -195,7 +195,7 @@ const Index2 = () => {
                   name="text"
                 />
               </div>
-              <div className="form_group">
+              {/* <div className="form_group">
                 <span>Accommodations</span>
                 <select className="wide">
                   <option data-display="Accommodations">Accommodations</option>
@@ -206,7 +206,7 @@ const Index2 = () => {
                   <option value={1}>Tent Camping</option>
                   <option value={1}>Couple Tent</option>
                 </select>
-              </div>
+              </div> */}
               <div className="form_group">
                 <button className="booking-btn">
                   Check Availability <i className="far fa-angle-double-right" />
@@ -240,7 +240,7 @@ const Index2 = () => {
                   />
                   <div className="item-overlay">
                     <div className="content">
-                      <h3 className="title">Tent Camping</h3>
+                      <h3 className="title">Hiking</h3>
                     </div>
                   </div>
                 </div>
@@ -256,7 +256,7 @@ const Index2 = () => {
                   />
                   <div className="item-overlay">
                     <div className="content">
-                      <h3 className="title">Mountain Biking</h3>
+                      <h3 className="title">Rock climbing</h3>
                     </div>
                   </div>
                 </div>
@@ -272,7 +272,7 @@ const Index2 = () => {
                   />
                   <div className="item-overlay">
                     <div className="content">
-                      <h3 className="title">Fishing</h3>
+                      <h3 className="title">Cultural tour</h3>
                     </div>
                   </div>
                 </div>
@@ -288,7 +288,7 @@ const Index2 = () => {
                   />
                   <div className="item-overlay">
                     <div className="content">
-                      <h3 className="title">Kayaking</h3>
+                      <h3 className="title">Bird watching</h3>
                     </div>
                   </div>
                 </div>
@@ -320,23 +320,23 @@ const Index2 = () => {
                 <div className="card-list">
                   <div className="card-item">
                     <i className="fas fa-badge-check" />
-                    Family Camping
+                    Bird watching
                   </div>
                   <div className="card-item">
                     <i className="fas fa-badge-check" />
-                    Wild Camping
+                    Hiking
                   </div>
                   <div className="card-item">
                     <i className="fas fa-badge-check" />
-                    Fishing
+                    Rock Climbing
                   </div>
                   <div className="card-item">
                     <i className="fas fa-badge-check" />
-                    Mountain Biking
+                    Coffee testing
                   </div>
                   <div className="card-item">
                     <i className="fas fa-badge-check" />
-                    Luxury Cabin
+                    Local Food
                   </div>
                   <div className="card-item">
                     <i className="fas fa-badge-check" />
@@ -549,227 +549,6 @@ const Index2 = () => {
         </div>
       </section>
       {/*====== End Services Section ======*/}
-      {/*====== Start Team Section ======*/}
-      <section className="team-section pt-100 pb-70">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-xl-7">
-              {/*====== Section Title ======*/}
-              <div className="section-title text-center mb-50 wow fadeInDown">
-                <span className="sub-title">Team Member</span>
-                <h2>We’ve Expert Team Members Meet With Team</h2>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xl-4 col-md-6 col-sm-12">
-              {/*====== Single Team Item ======*/}
-              <div className="single-team-item mb-30 wow fadeInUp">
-                <div className="member-img">
-                  <img src="assets/images/team/team-1.jpg" alt="Team Image" />
-                </div>
-                <div className="member-info">
-                  <h4 className="title">Donald D. Schafer</h4>
-                  <p className="position">CEO &amp; Founder</p>
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-facebook-f" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 col-sm-12">
-              {/*====== Single Team Item ======*/}
-              <div className="single-team-item mb-30 wow fadeInUp">
-                <div className="member-img">
-                  <img src="assets/images/team/team-2.jpg" alt="Team Image" />
-                </div>
-                <div className="member-info">
-                  <h4 className="title">William E. Combs</h4>
-                  <p className="position">Web Designer</p>
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-facebook-f" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 col-sm-12">
-              {/*====== Single Team Item ======*/}
-              <div className="single-team-item mb-30 wow fadeInUp">
-                <div className="member-img">
-                  <img src="assets/images/team/team-3.jpg" alt="Team Image" />
-                </div>
-                <div className="member-info">
-                  <h4 className="title">Daniel G. Santana</h4>
-                  <p className="position">Senior Manager</p>
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-facebook-f" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 col-sm-12">
-              {/*====== Single Team Item ======*/}
-              <div className="single-team-item mb-30 wow fadeInUp">
-                <div className="member-img">
-                  <img src="assets/images/team/team-4.jpg" alt="Team Image" />
-                </div>
-                <div className="member-info">
-                  <h4 className="title">Mario M. Troutman</h4>
-                  <p className="position">Web Developer</p>
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-facebook-f" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 col-sm-12">
-              {/*====== Single Team Item ======*/}
-              <div className="single-team-item mb-30 wow fadeInUp">
-                <div className="member-img">
-                  <img src="assets/images/team/team-5.jpg" alt="Team Image" />
-                </div>
-                <div className="member-info">
-                  <h4 className="title">Don O. Vaillancourt</h4>
-                  <p className="position">Junior Marketer</p>
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-facebook-f" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 col-sm-12">
-              {/*====== Single Team Item ======*/}
-              <div className="single-team-item mb-30 wow fadeInUp">
-                <div className="member-img">
-                  <img src="assets/images/team/team-6.jpg" alt="Team Image" />
-                </div>
-                <div className="member-info">
-                  <h4 className="title">Eugene J. Gilliam</h4>
-                  <p className="position">Graphics Designer</p>
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-facebook-f" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fab fa-linkedin-in" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/*====== End Team Section ======*/}
       {/*====== Start Activity Section ======*/}
       <section className="activity-section pb-60">
         <div className="container-fluid">
